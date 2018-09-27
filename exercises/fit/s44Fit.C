c95e57ae0b0ff83a8f8ed1a309c5134d1946caa5
Lew Riley <lriley@ursinus.edu>
2018-09-26 21:22:23 -0400

    Updated ./exercises

--- exercises/fit/s44Fit.C
+++ exercises/fit/s44Fit.C
@@ -1,124 +1,107 @@
-#include "Riostream.h"
-#include "iostream"
-#include "TVirtualFitter.h"
-#include "TRandom.h"
-#include "TFile.h"
 
-// These are declared in global scope so that they are accessible to the 
+// These must be declared in global scope so that they are accessible to the 
 // fit function fitf().
-GH1D *sim;       
+GH1D *sim[12];       
 GH1D *spectrum;
-GH1D *backGround;
 
 // Fit function
 Double_t fitf(Double_t *v,Double_t *par) {
 
   // Get bin contents at v[0]
-  Double_t bg    = backGround->GetBinContent(backGround->FindBin(v[0]));
-  Double_t sim0  = sim->GetBinContent(sim->FindBin(v[0]));
+  Double_t sim0  = sim[0]->GetBinContent(sim[0]->FindBin(v[0]));
+  Double_t sim1  = sim[1]->GetBinContent(sim[1]->FindBin(v[0]));
+  Double_t sim2  = sim[2]->GetBinContent(sim[2]->FindBin(v[0]));
+  Double_t sim3  = sim[3]->GetBinContent(sim[3]->FindBin(v[0]));
 
-  // Threshold
-  sim0 = sim0*(1.0 + tanh((v[0]-par[2])/par[3]))/2.0;
-
-  // Return a linear combination of the measured 
+  // Return a linear combination of two exponentials, the measured 
   // background and the simulated gamma rays.
-  Double_t value = par[0]*bg + par[1]*sim0;
+  Double_t value = exp(par[0]+par[1]*v[0]) + exp(par[2]+par[3]*v[0])
+    +  par[4]*sim0 +  par[5]*sim1 +  par[6]*sim2  +  par[7]*sim3;
 
   return value;
 
 }
 
-void eu152Fit(TString simFileName, Int_t rebin) {
+void s44Fit(Int_t rebin){
 
   // Load measured spectra =====================================================
-  cout << "\nLoading measured spectra ...\n" << endl;
+  cout << "\nLoading measured spectrum ...\n" << endl;
 
-  TString specFileName = "data/run020_cal_histos.root";
+  TString specFileName = "fakedata/fakedata.root";
   TFile *spF = new TFile(specFileName);
-  TH1F* spec;
-  spF->GetObject("egam", spec);
-  spectrum = (GH1D*)spec->Clone();
+  spF->GetObject("s44spectrum", spectrum);
   spectrum->Rebin(rebin);
-  
-  TString bgFileName   = "data/run007_cal_histos.root";
-  TFile *bgF = new TFile(bgFileName);
-  TH1F* back;
-  bgF->GetObject("egam", back);
-  backGround = (GH1D*)back->Clone();
-  backGround->Rebin(rebin);
-  
+
   // Load Geant4 simulations ===================================================
   cout << "\n... loading simulated spectra ...\n" << endl;
 
-  const Int_t nSim = 1;       // Number of simulated gamma-rays
-  
-  TFile *sF = new TFile(simFileName);
-  sF->GetObject("energy/energy", sim);
-  sim->Rebin(rebin);
-  
+  const Int_t nSim = 4;       // Number of simulated excited states
+  TString simFileName[nSim];
+  simFileName[0] = "s44_400_histos.root";
+  simFileName[1] = "s44_480_histos.root";
+  simFileName[2] = "s44_920_histos.root";
+  simFileName[3] = "s44_1400_histos.root";
+
+  for(int i=0;i<nSim;i++) {
+    TFile *sF = new TFile(simFileName[i]);
+    sF->GetObject("energy/dop_4169_gaus",sim[i]);
+    sim[i]->Rebin(rebin);
+  }
+
   // Check binning.
   cout << "\n   Measured spectrum has " << spectrum->GetBinWidth(1) 
        << " keV/channel" << endl;
-  cout << "   Simulations have "      << sim->GetBinWidth(1) 
+  cout << "   Simulations have "      << sim[0]->GetBinWidth(1) 
        << " keV/channel" << endl;
-  cout << "   These should match for meaninful absolute values of fit parameters.\n" << endl;
-
+  cout << "   These must match for meaninful absolute values of fit parameters.\n" << endl;
 
   // Fit =======================================================================
   cout << "\n... fitting ...\n" << endl;
 
-  const Int_t nPar = nSim+3;  // Number of fit parameters
-  Float_t bgMinE  = 1800.;
-  Float_t bgMaxE = 3000.;
-
-  Float_t fitMinE = 10.;
-  Float_t fitMaxE = 3000.;
+  const Int_t nPar = nSim+4;  // Number of fit parameters
+  Double_t fitMinE = 110.;
+  Double_t fitMaxE = 3000.;
 
   // Initialize fit parameter array
-  Double_t par[nPar];
-  par[0]  = 0.1;      // Measured random background
-  par[1]  = 10.0;     // Simulation
-  par[2]  = 85.;      // Threshold parameter 1
-  par[3]  =  5.;      // Threshold parameter 2
+  Double_t par[1000];
+  par[0]  = 1.;              // Exponential 1
+  par[1]  = -1E-02;
+  par[2]  = 1.;              // Exponential 2
+  par[3]  = -1E-03;
+  par[4]  = 0.001;           // 400 keV
+  par[5]  = 0.001;           // 480 keV
+  par[6]  = 0.001;           // 920 keV
+  par[7]  = 0.001;           // 1400 keV
 
-  TF1 *f1 = new TF1("f1", fitf, bgMinE, bgMaxE, nPar);
+  TF1 *f1 = new TF1("f1",fitf,fitMinE,fitMaxE,nPar);
   f1->SetLineColor(kBlue);
-  f1->SetNpx(10000);        // Number of points used to draw the function
+  f1->SetNpx(1000);     // Number of points used to draw the function
+  f1->SetLineWidth(2);
 
   f1->SetParameters(par);
 
-  // Fit the room background component.
-  f1->FixParameter(1, 10.);
-  f1->FixParameter(2, 85.);
-  f1->FixParameter(3, 5.);
-
-  spectrum->GetXaxis()->SetRangeUser(0.,3000.);
-  GH1D *diff = (GH1D*)spectrum->Clone("diff");
+  f1->SetParLimits(0, 0, 100);
+  f1->SetParLimits(1, -0.1, -0.005);
+  f1->SetParLimits(2, 0, 100);
+  f1->SetParLimits(3, -0.005, 0);
+  
+  spectrum->GetXaxis()->SetRangeUser(0., fitMaxE);
+  GH1D *diff    = (GH1D*)spectrum->Clone("diff");
   GH1D * sqrtup = (GH1D*)spectrum->Clone("Error+");
   GH1D * sqrtdn = (GH1D*)spectrum->Clone("Error-");
 
   ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2", "Migrad");
   ROOT::Math::MinimizerOptions::SetDefaultMaxFunctionCalls( 1000000 );
   ROOT::Math::MinimizerOptions::SetDefaultMaxIterations( 100000 );
-
-  spectrum->Fit("f1","LR");
-  
-  f1->FixParameter(0, f1->GetParameter(0));
-
-  // Fit the full spectrum.
-  f1->SetRange(fitMinE, fitMaxE);
-
-  f1->ReleaseParameter(1);
-  f1->ReleaseParameter(2);
-  f1->ReleaseParameter(3);
+   
   spectrum->Fit("f1","LR");
 
   // Discrepancy ===============================================================
   
-  // Create the difference spectrum
+  // Create the difference spectrum and channel error spectra
   for(int i=0;i<diff->GetNbinsX();i++) {
 
-    Float_t E = diff->GetBinCenter(i);
+    Double_t E = diff->GetBinCenter(i);
 
     if(E>fitMinE && E<fitMaxE) {
       diff->SetBinContent(i, f1->Eval(E) - diff->GetBinContent(i));
@@ -143,7 +126,7 @@ void eu152Fit(TString simFileName, Int_t rebin) {
   Spectra->Divide(1,2);
 
   Spectra->cd(1);
-  Spectra_1->SetLogy();
+  gPad->SetLogy();
 
   spectrum->SetStats(kFALSE);
   spectrum->SetLineColor(kBlack);
@@ -159,7 +142,6 @@ void eu152Fit(TString simFileName, Int_t rebin) {
   diff->GetYaxis()->SetTitle(Form("Counts/(%d keV)", rebin));
   diff->SetFillColor(kBlue);
   diff->SetLineColor(kBlue);
-  diff->GetYaxis()->SetRangeUser(-5000.,5000.);
   diff->Draw();
 
   sqrtup->SetFillColor(kGray);
@@ -170,14 +152,8 @@ void eu152Fit(TString simFileName, Int_t rebin) {
   sqrtdn->Draw("SAME");
   diff->Draw("SAME");
 
-  cout << "Total difference = " << diff->Integral() << endl;
-
 }
 
-void eu152Fit() {
-  eu152Fit("eu152_gammas_histos.root", 1);
-}
-
-void eu152Fit(TString simFileName) {
-  eu152Fit(simFileName, 1);
+void s44Fit() {
+  s44Fit(8);
 }
