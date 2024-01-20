43ffe10798053d6fdd185316eeff345ebd8a2313
Lew Riley <lriley@ursinus.edu>
2024-01-20 16:48:53 -0500

    Minor fix to Makefiles for inbeam examples

--- include/Git_Hash.hh
+++ include/Git_Hash.hh
@@ -1,5 +1,5 @@
 #ifndef Git_Hash_h
 #define Git_Hash_h
-#define GIT_HASH "3ee0eefc768ff67305afd1ef3b76292107ddbd98"
+#define GIT_HASH "f573d71c24995fd692d65091d9f0807422f19bdc"
 #define GIT_BRANCH "geant4.10"
 #endif
